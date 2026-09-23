e999b5eb3d6431710d9045a63b0e49c95483b00e
eggeo-lab <eggeo.lab@gmail.com>
2026-09-23 07:17:34 -0700

    dominio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Barrancas-Country/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
